5b274b01aafd6a1d50f40a6730947b8ed819a389
sydneycherek <78374649+sydneycherek@users.noreply.github.com>
2022-09-21 14:39:02 -0500

    Update index.php

--- index.php
+++ index.php
@@ -7,30 +7,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
   </head>
   <body>
-<table class="table table-striped">
-  <thead>
-    <tr>
-      <th>ID</th>
-      <th>Name</th>
-    </tr>
-  </thead>
-  <tbody>
-    <?php
-$servername = "localhost";
-$username = "sydneych_homework3";
-$password = "BananaSunday";
-$dbname = "sydneych_homework3";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-    
-?>
-  </tbody>
-    </table>
     <h1>Hello, world!</h1>
     <form action="handlepost.php" method="get">
 Name: <input type="text" name="name"><br>
