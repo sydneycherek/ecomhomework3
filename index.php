e4e639878152b1d34f0fcfc49fb88aa757f1f74a
sydneycherek <78374649+sydneycherek@users.noreply.github.com>
2022-09-21 14:38:32 -0500

    Update index.php

--- index.php
+++ index.php
@@ -27,25 +27,7 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-
-$sql = "SELECT animal_id, animal_id from Animal";
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-  // output data of each row
-  while($row = $result->fetch_assoc()) {
-?>
-  <tr>
-    <td><?=$row["animal_id"]?></td>
-    <td><a href="animal-section.php?id=<?=$row["animal_id"]?>"><?=$row["animalname"]?></a></td>
-    <td><?=$row["animaltype"]?></td>
-  </tr>
-<?php
-  }
-} else {
-  echo "0 results";
-}
-$conn->close();
+    
 ?>
   </tbody>
     </table>
