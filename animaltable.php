dc4c047259e1ba487b3fb2dc4add04375df237c6
sydneycherek <78374649+sydneycherek@users.noreply.github.com>
2022-09-21 13:53:20 -0500

    Update animaltable.php

--- animaltable.php
+++ animaltable.php
@@ -1,3 +1,25 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+  </head>
+  <body>
+<table class="table table-striped">
+  <thead>
+    <tr>
+      <th>ID</th>
+      <th>Name</th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php
+$servername = "localhost";
+$username = "sydneych_homework3";
+$password = "BananaSunday";
+$dbname = "sydneych_homework3";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
