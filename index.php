0b5851485eccfe445f7d838b971e697681f775df
sydneycherek <78374649+sydneycherek@users.noreply.github.com>
2022-09-26 12:56:08 -0500

    Update index.php

--- index.php
+++ index.php
@@ -30,6 +30,9 @@
         <li class="nav-item">
           <a class="nav-link" href="employeetable.php">Employee Page</a>
         </li>
+        <li class="nav-item">
+          <a class="nav-link" href="animal-employeetable.php">Join Table</a>
+        </li>
       </ul>
     </div>
   </div>
@@ -41,7 +44,7 @@ E-mail: <input type="text" name="email"><br>
 <input type="submit">
 </form>
     
-    <a class="btn btn-primary" href="page2.html" role="button">Page 2</a>
+    
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
   </body>
 </html>
