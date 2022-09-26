218fcb012d0c8f2f23b7e44bf29faf4cc0e69308
sydneycherek <78374649+sydneycherek@users.noreply.github.com>
2022-09-26 12:53:30 -0500

    Update index.php

--- index.php
+++ index.php
@@ -12,23 +12,23 @@
     
     <nav class="navbar navbar-expand-lg bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Home</a>
+    <a class="navbar-brand" href="index.php">Home</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Animal Page</a>
+          <a class="nav-link active" aria-current="page" href="animaltable.php">Animal Page</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Enclosure Page</a>
+          <a class="nav-link" href="enclosuretable.php">Enclosure Page</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Care Page</a>
+          <a class="nav-link" href="caretable.php">Care Page</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Employee Page</a>
+          <a class="nav-link" href="employeetable.php">Employee Page</a>
         </li>
       </ul>
     </div>
