be54940142bfa5f1ca83e40b984a8c7dd8a41fcc
sydneycherek <78374649+sydneycherek@users.noreply.github.com>
2022-09-28 13:29:05 -0500

    Update postpage.php

--- postpage.php
+++ postpage.php
@@ -23,37 +23,27 @@ if ($conn->connect_error) {
 }
 
 $cid=$_POST['id']
-$sql = "SELECT animal_id, animalname from Animal";
+$sql = "SELECT animalname, c.servicetype FROM Animal a JOIN Care c on c.animal_id = a.animal_id where a.animal_id = " .$cid;
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
 ?>
-   <div class="card">
-    <div class="card-body">
-      <h5 class="card-title"><?=$row["animalname"]?></h5>
-      <p class="card-text"><ul>
+  <tr>
+    <td><?=$row["animalname"]?></td>
+    <td><?=$row["servicetype"]?></td>
+  </tr>
 <?php
-    $section_sql = "select c.servicetype from Care c join Animal a on c.animal_id = a.animal_id where c.animal_id =" . $row["animal_id"];
-                                        //echo $section_sql;
-    $section_result = $conn->query($section_sql);
     
-    while($section_row = $section_result->fetch_assoc()) {
-      echo "<li>" . $section_row["servicetype"] . "</li>";
-    }
-?>
-      </ul></p>
-  </div>
-    </div>
-<?php
   }
 } else {
   echo "0 results";
 }
 $conn->close();
 ?>
-  </card-group>
+  </tbody>
+    </table>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
   </body>
