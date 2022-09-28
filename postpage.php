11c7cf2d0188377f3cf0b77a00d0cf07a0c7c6eb
sydneycherek <78374649+sydneycherek@users.noreply.github.com>
2022-09-28 14:04:29 -0500

    Update postpage.php

--- postpage.php
+++ postpage.php
@@ -1,14 +1,25 @@
 <!doctype html>
 <html lang="en">
   <head>
+  <!doctype html>
+<html lang="en">
+  <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Instructors</title>
+    <title>Sections</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
   </head>
   <body>
     <h1>Animals and their Care</h1>
-<div class="card-group">
+<table class="table table-striped">
+  <thead>
+    <tr>
+      <th>Animal ID</th>
+      <th>Name</th>
+      <th>Service Type</th>
+    </tr>
+  </thead>
+  <tbody>
     <?php
 $servername = "localhost";
 $username = "sydneych_homework3";
@@ -21,21 +32,22 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-
-$cid=$_POST['id']
-$sql = "SELECT animalname, c.servicetype FROM Animal a JOIN Care c on c.animal_id = a.animal_id where a.animal_id = " .$cid;
-$result = $conn->query($sql);
+$cid = $_POST['id'];
+//echo $iid;
+$sql = "select  animal_id, animalname, servicetype FROM Animal a JOIN Care c ON a.animal_id = c.animal_id WHERE a.animal_id = " . $cid;
+//echo $sql;
+    $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
 ?>
   <tr>
+    <td><?=$row["animal_id"]?></td>
     <td><?=$row["animalname"]?></td>
     <td><?=$row["servicetype"]?></td>
   </tr>
 <?php
-    
   }
 } else {
   echo "0 results";
@@ -44,7 +56,6 @@ $conn->close();
 ?>
   </tbody>
     </table>
-
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
   </body>
 </html>
